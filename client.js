bfd3c30399c957819f35cdd0fd561e5bacd65f9e
DEV-H <houda.mallem@gmail.com>
2017-08-10 15:23:52 +0100

    template

--- client.js
+++ client.js
@@ -3,14 +3,21 @@
    var socket = io.connect("http://localhost:1337");
     $('#loginform').submit(function(event){
         event.preventDefault();
-        socket.emit('login' , {
-            username : $('#username').val(),
-            mail : $('#mail').val()
-        })
+        if($('#username').val() !='' && $('#mail').val()!=''){
+            socket.emit('login' , {
+                username : $('#username').val(),
+                mail : $('#mail').val()
+            })
+        }else{
+            alert('veuillez insérer le nom et l email svp')
+        }
+
         });
         socket.on('newuser' , function(user){   
           
-            $('#users').append('<img src="'+user.avatar+'" id="'+user.id+'"/>')
+            $('#users').append('<img width="20px; " src="'+user.avatar+'" id="'+user.id+'"/> ')
+                    $('#users').append('<strong>'+user.username+'</strong>')
+                            $('#users').append('<br/> ')
         
         });
             socket.on('logged' , function(user){   
@@ -29,17 +36,42 @@
         
         });
         socket.on('newmsg' , function(message){   
-           // alert("new user");
-            $('#divmessage').append("<div class='ui olive segment' >"+message.message+"</div>");
-        
+            console.log(message);
+         //   $('#divmessage').append("<div class='ui olive segment onemesssage' >");
+                    var blockmessage=document.getElementById("divmessage"); 
+                    var div = document.createElement('div');
+                        blockmessage.appendChild(div);
+                        div.setAttribute("class",'ui olive segment');
+                    var image = document.createElement('img');
+                        image.setAttribute("width",'20');
+                        image.setAttribute("style",'margin-right:20px');
+                        image.setAttribute("src",message.user.avatar);
+                        div.append(image);
+                        div.append(message.user.username);
+                        var br = document.createElement('br');
+                        div.appendChild(br);
+                        div.append(message.message);
+
+      /*        var newDiv = document.createElement("div"); 
+  var newContent = document.createTextNode("Hi there and greetings!"); 
+  newDiv.appendChild(newContent);
+                   $('.onemesssage').append('<img width="20px; " src="'+message.user.avatar+'" /> ')
+                   $('.onemesssage').append(message.user.username);
+                            $('.onemesssage').append("</br>");
+                        $('.onemesssage').append(message.message);
+                           $('#divmessage').append("</div >");
+        */
         });
-            $('#mesfor').submit(function(event){
-        event.preventDefault();
-              socket.emit('newmsg' , {
-            message : $('#message').val()
-        })
-        $('#message').val('');
-        $('#message').focus();
+        $('#mesfor').submit(function(event){
+            if( $('#message').val() !=''){
+                event.preventDefault();
+                    socket.emit('newmsg' , {
+                    message : $('#message').val()
+                })
+                $('#message').val('');
+                $('#message').focus();
+            }
+
   
         });
 })(jQuery);
